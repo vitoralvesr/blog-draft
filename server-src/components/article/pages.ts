d9930d5974e9f6388523b1f8472c76929be2375e
Willian Krueger <wkrueger128@gmail.com>
2017-01-23 02:34:29 -0200

    page title

--- server-src/components/article/pages.ts
+++ server-src/components/article/pages.ts
@@ -33,7 +33,7 @@ pages.get('/list', async (req, res, next) => {
         })
         var rows2 = await Promise.all(all)
         res.render('v-list', {
-            title : 'Blog' ,
+            title : config.main_title ,
             itens: rows2,
             $atRoot : true
         })
