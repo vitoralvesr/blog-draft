e011f0a16c016ff2e6664e7f51f24a1d62861c6a
Willian Krueger <wkrueger128@gmail.com>
2017-01-23 02:49:41 -0200

    fix demo mode

--- server-src/common/auth-mw.ts
+++ server-src/common/auth-mw.ts
@@ -17,10 +17,40 @@ export var initSession = expressSession({
 })
 
 
+if (process.env.APP_DEMO_MODE) {
+    initSession = combineMiddleware([
+        initSession,
+        (req, res, next) => {
+            ;(req.session || {}).userId = Number(process.env.APP_DEMO_MODE)
+        }
+    ])
+}
+
 export var authGate = (req, res, next) => {
     if (process.env.APP_DEMO_MODE) {
         req.session.userId = Number(process.env.APP_DEMO_MODE)
     }
     if (req.session.userId) return next()
     return next(ono({statusCode:400, code:'UNAUTHORIZED'}, 'Não autorizado.'))
+}
+
+
+/**
+ * Combine multiple middleware together.
+ *
+ * @param {Function[]} mids functions of form:
+ *   function(req, res, next) { ... }
+ * @return {Function} single combined middleware
+ */
+function combineMiddleware(mids) {
+  return mids.reduce(function(a, b) {
+    return function(req, res, next) {
+      a(req, res, function(err) {
+        if (err) {
+          return next(err);
+        }
+        b(req, res, next);
+      });
+    };
+  });
 }
